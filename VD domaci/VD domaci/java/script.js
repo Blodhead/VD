1b9d7ad468a286d06a7bbbc5ca16ad06358f6d49
Blodhead <81102425+Blodhead@users.noreply.github.com>
2021-05-22 22:06:50 +0200

    Update 1.1

---
+++ VD domaci/VD domaci/java/script.js
@@ -0,0 +1,12 @@
+var counter = 60;
+var interval = setInterval(function() {
+    counter--;
+    // Display 'counter' wherever you want to display it.
+    if (counter <= 0) {
+     		clearInterval(interval);
+      	$('#timer').html("<h3>Count down complete</h3>");  
+        return;
+    }else{
+    	$('#time').text(counter);
+    }
+}, 1000);
