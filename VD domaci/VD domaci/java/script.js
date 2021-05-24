e008608bf8561b0c23fb899abce74c047e6f98ae
Blodhead <81102425+Blodhead@users.noreply.github.com>
2021-05-24 14:02:31 +0200

    Update 3.0 Igra radi

--- VD domaci/VD domaci/java/script.js
+++ VD domaci/VD domaci/java/script.js
@@ -9,13 +9,13 @@ var model = {
     
     matrix : [
 
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
-    {pokusaj : [0,0,0,0], pogodak : [0,0,0,0], cnt : 0},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
+    {pokusaj : [0,0,0,0], pogodak : [false,false,false,false]},
 
 ]
 }
@@ -82,7 +82,7 @@ function initPodesavanja(){
     document.getElementById('Slavonija').addEventListener('click',addElement);
     document.getElementById('RepublikaRrspka').addEventListener('click',addElement);
 
-    document.getElementById('Potvrdi').addEventListener('click',endTurn);
+    document.getElementById('Potvrdi').addEventListener('click',endPodesavanjaTurn);
 
 }
 ///////////////////////////////////////////////////////////
@@ -142,7 +142,7 @@ function removeElement(){
 
 ///////////////////////////////////////////////////////////
 
-function endTurn(){
+function endPodesavanjaTurn(){
 
     if(turn == "Cvarkov") {
 
@@ -175,7 +175,7 @@ function endTurn(){
         document.getElementById('Slavonija').removeEventListener('click',addElement);
         document.getElementById('RepublikaRrspka').removeEventListener('click',addElement);
     
-        document.getElementById('Potvrdi').removeEventListener('click',endTurn);
+        document.getElementById('Potvrdi').removeEventListener('click',endPodesavanjaTurn);
 
         window.location.href = "../html/skocko-igra.html";
 
@@ -211,20 +211,8 @@ function initIgra(){
     cvarkov = JSON.parse(localStorage.getItem('Igrac1'));
     boskic = JSON.parse(localStorage.getItem('Igrac2'));
 
-    document.getElementById('Skocko1').addEventListener('click',addSign);
-    document.getElementById('Kordun1').addEventListener('click',addSign);
-    document.getElementById('Lika1').addEventListener('click',addSign);
-    document.getElementById('Banija1').addEventListener('click',addSign);
-    document.getElementById('Slavonija1').addEventListener('click',addSign);
-    document.getElementById('RepublikaRrspka1').addEventListener('click',addSign);
 
-    document.getElementById('dd0').addEventListener('click',check);
-    document.getElementById('dd1').addEventListener('click',check);
-    document.getElementById('dd2').addEventListener('click',check);
-    document.getElementById('dd3').addEventListener('click',check);
-    document.getElementById('dd4').addEventListener('click',check);
-    document.getElementById('dd5').addEventListener('click',check);
-    document.getElementById('dd6').addEventListener('click',check);
+    buttonsON(true);
 
 }
 
@@ -234,40 +222,18 @@ function addSign(){
 
     var icon = this.id; 
 
-    //switch(turn){
 
-        //case "Cvarkov":{
+    for(var i = 0; i < 4; i++){
 
-            for(var i = 0; i < 4; i++){
-
-                if(model.matrix[row].pokusaj[i] == 0){
-                    icon = icon.slice(0,-1);
-                    model.matrix[row].pokusaj[i] = icon;
-                    document.getElementById(""+ row + i).innerHTML = "<img class=\"icons\" id="+ "2" + row + (i+1) +" onclick=\"\" src=\"../Images/" + icon + ".png\" alt=\"\">"
-                    document.getElementById("2" + row + (i+1)).addEventListener('click',removeSign);
-                    break;
-                }
-
-            }
- //           break;
-  //      }
-   /*     case "Boskic":{
-
-            for(var i = 0; i < 4; i++){
-
-                if(boskic[i] == 0){
-                    icon = icon.slice(0,-1);
-                    model.matrix[row].pokusaj[i] = icon;
-                    document.getElementById(""+ row + i).innerHTML = "<img class=\"icons\" id="+ (i+1) +" onclick=\"\" src=\"../Images/" + icon + ".png\" alt=\"\">"
-                    document.getElementById("" + (i+1)).addEventListener('click',removeSign);
-                    break;
-                }
-
-            }
+        if(model.matrix[row].pokusaj[i] == 0){
+            icon = icon.slice(0,-1);
+            model.matrix[row].pokusaj[i] = icon;
+            document.getElementById(""+ row + i).innerHTML = "<img class=\"icons\" id="+ "2" + row + (i+1) +" onclick=\"\" src=\"../Images/" + icon + ".png\" alt=\"\">"
+            document.getElementById("2" + row + (i+1)).addEventListener('click',removeSign);
             break;
-        }*/
-    //}
+        }
 
+    }
 
 }
 ///////////////////////////////////////////////////////////
@@ -283,85 +249,182 @@ function removeSign(){
 
 ///////////////////////////////////////////////////////////
 function check(){ 
-    col = 0;
-    var temp = 0;
+
     switch(turn){
 
-        case "Cvarkov":{
-
-            for(var i = 0; i < 4; i++){
-
-                if(model.matrix[row].pokusaj[i] == cvarkov[i]) {model.matrix[row].pogodak[i] = 1; temp++;}
-                else model.matrix[row].pogodak[i] = 0;
-
-            }
-
-            for(var i = 0; i < 4; i++){
-                for(var j = 0; j < 4; j++){
-
-                    if(model.matrix[row].pogodak[i] == 1) break;
-                    if(model.matrix[row].pokusaj[i] == cvarkov[j])
-                    model.matrix[row].pogodak[i] = 2;
-
-                }
-            }
-            var j = 0;
-            for(var i = 0 ; i < 4 ; i++){
-                
-                if(model.matrix[row].pogodak[i] == 1){
-                    document.getElementById('1' + row + i).style.backgroundColor = "red";
-                    j++;
-                }
-                else if(model.matrix[row].pogodak[i] == 2)
-                    document.getElementById('1' + row + j).style.backgroundColor = "yellow";
-            }
-
-            document.getElementById("dd" + row).disabled = true;
-
-            row++;
-
-            break;
-            
-        }
-        case "Boskic":{
-
-            for(var i = 0; i < 4; i++){
-
-                if(model.matrix[row].pokusaj[i] == boskic[i]) {model.matrix[row].pogodak[i] = 1; temp++;}
-                else model.matrix[row].pogodak[i] = 0;
-
-            }
-
-            for(var i = 0; i < 4; i++){
-                for(var j = 0; j < 4; j++){
-
-                    if(model.matrix[row].pogodak[i] == 1) break;
-                    if(model.matrix[row].pokusaj[i] == boskic[j])
-                    model.matrix[row].pogodak[i] = 2;
-
-                }
-            }
-            var j = 0;
-            for(var i = 0 ; i < 4 ; i++){
-                
-                if(model.matrix[row].pogodak[i] == 1){
-                    document.getElementById('1' + row + i).style.backgroundColor = "red";
-                    j++;
-                }
-                else if(model.matrix[row].pogodak[i] == 2)
-                    document.getElementById('1' + row + j).style.backgroundColor = "yellow";
-            }
-
-            document.getElementById("dd" + row).disabled = true;
-
-            row++;
-
-            break;
-        }
+        case "Cvarkov":{provera(cvarkov); break;}
+        case "Boskic":{provera(boskic); break;}
     }
 
+    endTurn();
 
 }
+//////////////////////////////////////////////////////////
+
+function provera(igr){
+    var correct = 0, wrong = 0;
+    {
+        var igrac = {
+
+            matrix : [
+        
+            {kombinacija:[0,0,0,0], pogodak : [0,0,0,0]}            
+        ]
+        }
+
+        igrac.matrix[0].kombinacija = igr;
+
+        for(var i = 0; i < 4; i++){
+
+            if(model.matrix[row].pokusaj[i] == igrac.matrix[0].kombinacija[i]) {
+                model.matrix[row].pogodak[i] = true; 
+                igrac.matrix[0].pogodak[i] = 1;
+                correct++;
+            }
+
+        }
+
+        for(var i = 0; i < 4; i++){
+
+           // if(igrac.matrix[0].pogodak[i] == 1 || igrac.matrix[0].pogodak[i] == 2) continue;
+
+            for(var j = 0; j < 4; j++){
+                
+                if(model.matrix[row].pogodak[j] == false)
+                if(igrac.matrix[0].kombinacija[i] == model.matrix[row].pokusaj[j]){
+                    model.matrix[row].pogodak[j] = true;
+                    igrac.matrix[0].pogodak[j] = 2;
+                    wrong++;
+                    break;
+                }
+            }
+        }
+
+        for(var i = 0 ; i < 4 ; i++){
+            
+            if(correct-- > 0){
+                document.getElementById('1' + row + i).style.backgroundColor = "red";
+
+            }
+            else if(wrong-- >0)
+
+                document.getElementById('1' + row + i).style.backgroundColor = "yellow";
+        }
+
+        document.getElementById("dd" + row).disabled = true;
+
+        row++;
+        
+    }
+
+}
+
+
+///////////////////////////////////////////////////////////
+
+function endTurn(){
+
+    var temp = 0;
+
+    for(var i = 0; i < 4 ; i++){
+        if(document.getElementById("1" + (row-1) + i).style.backgroundColor == "red") temp++;
+    }
+
+    if(temp == 4) {
+
+        buttonsON(false);
+        row--;
+        document.getElementById("Sledeci").innerHTML = "Освијили сте: " + pobeda + " поена! <br> <button id=\"Ddugme\" class=\"btn btn-primary\" style=\"width: 150px; border: black; border: 2px solid black; font-size: medium; font-family:cursive; color: white;\">Следећи играч</button>"
+        document.getElementById("Ddugme").addEventListener('click',nextPlayer);
+    }
+
+}
+
+function nextPlayer(){
+
+    if(turn = "Cvarkov"){
+        turn = "Boskic"
+        document.getElementById('Start').disabled = false;
+    }
+
+    while(row>=0){
+        for(var i = 0; i < 4 ; i++){
+        
+        model.matrix[row].pokusaj[i] = 0;
+        model.matrix[row].pogodak[i] = false;
+
+        document.getElementById(""+ row + i).innerHTML = "";
+        document.getElementById('1' + row + i).style.backgroundColor = "rgb(139, 136, 136)";
+
+        }
+        row--;
+    }
+
+    row=0;
+    document.getElementById("Sledeci").innerHTML = "";
+    buttonsON(true);
+
+}
+
+///////////////////////////////////////////////////////////
+
+function buttonsON(bool){
+
+    if(bool == true){
+
+        document.getElementById('dd0').disabled = false;
+        document.getElementById('dd1').disabled = false;
+        document.getElementById('dd2').disabled = false;
+        document.getElementById('dd3').disabled = false;
+        document.getElementById('dd4').disabled = false;
+        document.getElementById('dd5').disabled = false;
+        document.getElementById('dd6').disabled = false;
+
+        document.getElementById('dd0').addEventListener('click',check);
+        document.getElementById('dd1').addEventListener('click',check);
+        document.getElementById('dd2').addEventListener('click',check);
+        document.getElementById('dd3').addEventListener('click',check);
+        document.getElementById('dd4').addEventListener('click',check);
+        document.getElementById('dd5').addEventListener('click',check);
+        document.getElementById('dd6').addEventListener('click',check);
+
+        document.getElementById('Skocko1').addEventListener('click',addSign);
+        document.getElementById('Kordun1').addEventListener('click',addSign);
+        document.getElementById('Lika1').addEventListener('click',addSign);
+        document.getElementById('Banija1').addEventListener('click',addSign);
+        document.getElementById('Slavonija1').addEventListener('click',addSign);
+        document.getElementById('RepublikaRrspka1').addEventListener('click',addSign);
+
+    }
+    else if(bool == false){
+
+        document.getElementById('dd0').disabled = true;
+        document.getElementById('dd1').disabled = true;
+        document.getElementById('dd2').disabled = true;
+        document.getElementById('dd3').disabled = true;
+        document.getElementById('dd4').disabled = true;
+        document.getElementById('dd5').disabled = true;
+        document.getElementById('dd6').disabled = true;
+
+        document.getElementById('dd0').removeEventListener('click',check);
+        document.getElementById('dd1').removeEventListener('click',check);
+        document.getElementById('dd2').removeEventListener('click',check);
+        document.getElementById('dd3').removeEventListener('click',check);
+        document.getElementById('dd4').removeEventListener('click',check);
+        document.getElementById('dd5').removeEventListener('click',check);
+        document.getElementById('dd6').removeEventListener('click',check);
+
+        document.getElementById('Skocko1').removeEventListener('click',addSign);
+        document.getElementById('Kordun1').removeEventListener('click',addSign);
+        document.getElementById('Lika1').removeEventListener('click',addSign);
+        document.getElementById('Banija1').removeEventListener('click',addSign);
+        document.getElementById('Slavonija1').removeEventListener('click',addSign);
+        document.getElementById('RepublikaRrspka1').removeEventListener('click',addSign);
+
+    }
+
+}
+
 
 ///////////////////////////////////////////////////////////
                 /////// TIMER ///////
